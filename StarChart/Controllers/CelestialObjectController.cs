8f7a9872b4cc37872fa960755e50c2d9430cd781
rpitogo@fullscale.io <rpitogo@fullscale.io>
2022-07-13 23:30:26 +0800

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -12,6 +15,38 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             this._context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var o = this._context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+            if (o == null)
+                return NotFound();
+            o.Name = "GetById";
+            o.Satellites.Select(x => x.Satellites.Select(y => y = new CelestialObject()));
+            return Ok(o);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var o = this._context.CelestialObjects.FirstOrDefault(x => x.Name == name);
+            if (o == null)
+                return NotFound();
+
+            o.Satellites.Select(x => x.Satellites.Select(y => y = new CelestialObject()));
+            return Ok(o);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var o = this._context.CelestialObjects.ToList();
+            if (o == null)
+                return NotFound();
+            o.Select(x => x.Satellites.Select(y => y = new CelestialObject()));
+            return Ok(o);
         }
     }
 }
